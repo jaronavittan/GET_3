53f5eeb751113722197e0f32157bed34b26b865e
Jaron Avittan <96601217+jaronavittan@users.noreply.github.com>
2023-06-07 09:23:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-    $name = $_GET['name'];
-    $surname = $_GET['surname'];
+   
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-      <title> Get and Post explained </title>
-    </head>
-    <body>
-
-      <h2> Handling your data </h2>
-      <?php echo 'your name is', $name, ' ',$surname ?>
-    </body>
-
+  <head>
+    <title> Get and Post explained </title>
+  </head>
+  <body>
+      <form action="handler.php" method="get">
+        name:<input type="text"name="name"/>
+        surname:<input type="text" name="surname"/>
+        <input type="submit" value="submit">
+      </form>  
+  </body>
   
 </html>
